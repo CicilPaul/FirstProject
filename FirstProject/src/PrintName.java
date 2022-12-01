a97ffa05ee068b6f81230e36d668c9fe42c8eecf
CicilPaul <bosenija10@gmail.com>
2022-12-01 06:51:02 -0800

    This is first commit

---
+++ FirstProject/src/PrintName.java
@@ -0,0 +1,21 @@
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		int age;
+		char letter='$';
+		double decimal=89.10;
+		boolean var=false;
+		age=65;
+		String name="Nija is so sweet";
+		System.out.println(age);
+		System.out.println(letter);
+		System.out.println(name);
+		System.out.println(decimal);
+		System.out.println(var);
+		
+				
+
+	}
+
+}
